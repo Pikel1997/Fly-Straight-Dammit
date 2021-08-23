c110416abab61023254c03aac30bd5a4bd576561
Pikel1997 <76587209+Pikel1997@users.noreply.github.com>
2021-08-23 14:27:07 +0400

    Absolute dogshit attempt

--- fly-straight-dammit.py
+++ fly-straight-dammit.py
@@ -1,1 +1,23 @@
-
+# import numpy as np
+# import matplotlib.pyplot as plt
+#
+#
+# def hcf(i, j):
+#     if j == 0:
+#         return i
+#     else:
+#         return hcf(j, i % j)
+#
+#
+# def fly_straight(n):
+#     l = []
+#     z = []
+#     for i in range(1, n):
+#         l.append(i)
+#
+#
+# a(n)=a(n-1)+n+1
+# a(n)=a(n-1)/gcd(n,a(n-1)) if gcd>1
+# where n = i and a(n) = j
+#
+# print(fly_straight(8))
