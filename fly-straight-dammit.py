79f21f940c3facd6b302375dbd2fbc6efd4df8e6
Pikel1997 <76587209+Pikel1997@users.noreply.github.com>
2021-08-28 19:01:50 +0400

    Final Commits with all the comments

--- fly-straight-dammit.py
+++ fly-straight-dammit.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+# recursive function to find hcf of 2 numbers
 def hcf(i, j):
     if j == 0:
         return i
@@ -9,6 +10,11 @@ def hcf(i, j):
         return hcf(j, i % j)
 
 
+# n = [0,1,2,3,4]
+# a(n)=[1,1,4,8,2]
+# a(n)=a(n-1)+n+1
+# if hcf>1, a(n)=a(n-1)/hcf(n,a(n-1))
+# since l[n] = n if l = range[0 to n] substitute with x
 def fly_straight(v):
     global n, an
     n = [0, 1]
@@ -17,15 +23,16 @@ def fly_straight(v):
         n.append(x)
         try:
             if hcf(x, an[x - 1]) > 1:
-                an.append(an[x - 1]/hcf(x, an[x - 1]))
+                an.append(an[x - 1] / hcf(x, an[x - 1]))
             else:
                 an.append(an[x - 1] + x + 1)
         except IndexError:
-            # The 2 functions above return an index error due to z[i-1],
+            # The 2 equations above return an index error due to z[i-1],
             # where if i = 0 then z[-1] is not a valid index value
             continue
 
 
+# calling the main function and plotting it
 def plot_points():
     fly_straight(2000)
     plt.scatter(np.array(n), np.array(an), s=1)
