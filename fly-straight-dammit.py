68d37b4c0c80a7ce328d48448709c261a6c23057
Pikel1997 <76587209+Pikel1997@users.noreply.github.com>
2021-08-28 18:51:26 +0400

    Final Commit

--- fly-straight-dammit.py
+++ fly-straight-dammit.py
@@ -9,31 +9,33 @@ def hcf(i, j):
         return hcf(j, i % j)
 
 
-def fly_straight(n):
-    l = [0, 1]
-    z = [1]
-    for i in range(0, n):
-        l.append(i)
+def fly_straight(v):
+    global n, an
+    n = [0, 1]
+    an = [1, 1]
+    for x in range(2, v):
+        n.append(x)
         try:
-            if hcf(l[i], z[i-1]) > 1:
-                j = z[i-1]/hcf(i, z[i-1])
+            if hcf(x, an[x - 1]) > 1:
+                an.append(an[x - 1]/hcf(x, an[x - 1]))
             else:
-                j = z[i-1] + i + 1
-                z.append(j)
+                an.append(an[x - 1] + x + 1)
         except IndexError:
-            pass
-    print(z)
-    print(l)
+            # The 2 functions above return an index error due to z[i-1],
+            # where if i = 0 then z[-1] is not a valid index value
+            continue
 
 
-# a(n)=a(n-1)+n+1
-# 8 = 4+3+1
-# n = [0,1,2,3,4]
-# a(n)=[1,1,4(2),8(3),2]
-# a(n)=a(n-1)/gcd(n,a(n-1)) if hcf>1
-# where n = i and a(n) = j
+def plot_points():
+    fly_straight(2000)
+    plt.scatter(np.array(n), np.array(an), s=1)
 
-print(fly_straight(10))
 
-# hcf(1,3) j!= 0
-# (3,1)
+# plotting
+plt.title("Fly Straight Dammit")
+plt.xlabel("n")
+plt.ylabel("a[n]")
+plot_points()
+plt.gca().spines['top'].set_visible(False)
+plt.gca().spines['right'].set_visible(False)
+plt.show()
